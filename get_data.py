ac68c4fe8931a10a6612e2f2657d892a2401b6c1
yokowork <68415792+yokowork@users.noreply.github.com>
2021-08-26 00:09:02 +0800

    Try

--- get_data.py
+++ get_data.py
@@ -37,7 +37,7 @@ def get_interest(company, timeframe):  #  base function to get 'interest' from G
     pytrend.build_payload(kw_list=[company], timeframe=timeframe, geo='TW')  
     # finding interest for 'company' during 'timeframe'
     result = pytrend.interest_over_time().drop('isPartial', axis=1)  # saving the 'interest' values
-
+    print(result)
     return result
 
 
